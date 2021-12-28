48ac9efcb03d5375f879a1947ab5c86abd2ecc8d
JKGriffon <scat_21@mail.ru>
2021-12-28 13:09:57 +0300

    Release 1

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ student.firstName = 'Petya'
 delete student.firstName
 
 console.log(student)
+
+const group = [
+  {firstName: 'Petya', lastNAme: 'Perviy'},
+  {firstName: 'Misha', lastNAme: 'Vtoroy'},
+  {firstName: 'Vasia', lastNAme: 'Tretiy'},
+]
+
+
+
+
+
+
